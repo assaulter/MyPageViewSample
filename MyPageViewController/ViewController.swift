0f280caf6c4e194c4933522271697e7de8d436f6
Kazuki Kubo <kz@assaulter.org>
2019-06-14 00:01:21 +0900

    Initial Commit

---
+++ MyPageViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPageViewController
+//
+//  Created by Kazuki Kubo on 2019/06/14.
+//  Copyright © 2019 Kazuki Kubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
